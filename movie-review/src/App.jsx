d9ea75b5b28d88739493d1c5ee48ff65913846c1
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-13 11:45:46 +0900

    ✨ feat: 리액트로 전환

--- movie-review/src/App.jsx
+++ movie-review/src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import MovieList from "./components/MovieSection/MovieList";
 
 function App() {
   return (
     <>
       <div id="wrap">
         <Header />
+        <MovieList />
+        <Footer />
       </div>
     </>
   );
---
+++ movie-review/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer class="footer">
+      <p>&copy; 우아한테크코스 All Rights Reserved.</p>
+      <p>
+        <img src="./images/woowacourse_logo.png" width="180" />
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
